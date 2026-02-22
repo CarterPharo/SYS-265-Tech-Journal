f020b5e0c2fb4ab7e4b7bfc6f45a9daa296a414b
CarterPharo <112458091+CarterPharo@users.noreply.github.com>
2026-02-21 20:24:01 -0500

    Fix directory path for new SSH user's .ssh folder

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,7 +4,7 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
 sudo useradd -m -d /home/$1 -s /bin/bash $1
-sudo mkdir /home/$1E/.ssh
+sudo mkdir /home/$1/.ssh
 sudo cp ~/SYS-265-Tech-Journal/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
 sudo chmod 700 /home/$1/.ssh
 sudo chmod 600 /home/$1/.ssh/authorized_keys
